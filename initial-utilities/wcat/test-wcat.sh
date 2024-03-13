8e9e551cf6afe35746136444c3a495e3a65e94a7
souzajju <163307119+souzajju@users.noreply.github.com>
2024-03-13 10:29:56 -0300

    test-wcat.sh

--- initial-utilities/wcat/test-wcat.sh
+++ initial-utilities/wcat/test-wcat.sh
@@ -1,10 +1,20 @@
-#! /bin/bash
+#include <stdio.h>
+#include <stdlib.h> 
+#define BUFFER_SIZE (10)
 
-if ! [[ -x wcat ]]; then
-    echo "wcat executable does not exist"
-    exit 1
-fi
-
-../../tester/run-tests.sh $*
-
-
+int main (int argc, char*argv[])
+{
+    //FILE * fp = fopen (const char *restrict filename, const char *restrict mode);
+    FILE * fp = fopen (argv[1], "r");
+    if (fp == NULL)
+    {
+        printf("wcat: cannot open file\n");
+        exit(1);
+    }
+    char buffer [BUFFER_SIZE];
+        while (fgets(buffer, BUFFER_SIZE, fp) != NULL)
+        printf("%s\n", buffer);
+    
+    fclose (fp);
+    return 0;
+}
